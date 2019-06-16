5d1570010bbbd7cce08db1e42cfaca96b5091064
Chris Patuzzo <chris@patuzzo.co.uk>
2019-06-16 10:36:33 +0100

    Add a deploy script for GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 const withSass = require('@zeit/next-sass');
 
+const pathPrefix = process.env.NODE_ENV === 'production' ? '/tracker-demo' : '';
+
 module.exports = withSass({
-  cssModules: true
+  cssModules: true,
+  assetPrefix: pathPrefix,
+  publicRuntimeConfig: { pathPrefix }
 });
