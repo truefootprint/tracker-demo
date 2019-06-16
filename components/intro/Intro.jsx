f7ff3cf838e415ed0e0053cc6d5455315bbe40e9
Chris Patuzzo <chris@patuzzo.co.uk>
2019-06-16 13:53:24 +0100

    Use dynamic content for the intro page

--- components/intro/Intro.jsx
+++ components/intro/Intro.jsx
@@ -1,20 +1,22 @@
 import Layout from "../layout";
 import css from "./styles.scss";
 
-const Intro = ({ setPage }) => (
-  <Layout>
-    <div className={css.intro}>
-      <p>Hello G1,</p>
-      <p>&nbsp;</p>
-      <p>Asset manager XYZ invited you to provide them with outcome numbers using the TrueFootprint Tracker.</p>
-      <p>Read more <a>here</a> about why asset managers want to have this data.</p>
-      <p>The data you enter is yours. By submitting you give us a license to sell this data to XYZ and others. Read <a>here</a> the full T&C.</p>
-      <p>&nbsp;</p>
-      <p>We have already created a basic company profile for you based on data provided by XYZ.</p>
+const Intro = ({ content, setPage }) => {
+  const t = content.intro;
 
-      <a className={css.call_to_action} onClick={() => setPage("login")}>Start</a>
+  return <Layout>
+    <div className={css.intro}>
+      <p>{t.para1}</p>
+      <p>&nbsp;</p>
+      <p>{t.para2}</p>
+      <p>{t.para3}</p>
+      <p>{t.para4}</p>
+      <p>&nbsp;</p>
+      <p>{t.para5}</p>
+
+      <a className={css.call_to_action} onClick={() => setPage("login")}>{t.cta}</a>
     </div>
-  </Layout>
-);
+  </Layout>;
+};
 
 export default Intro;
--- static/content.js
+++ static/content.js
@@ -28,7 +28,15 @@ export default {
     cta2: `→ Forward`,
   },
   intro: {
+    para1: `Hello G1,`,
 
+    para2: `Asset manager XYZ invited you to provide them with outcome numbers using the TrueFootprint Tracker.`,
+    para3: `Read more <a>here</a> about why asset managers want to have this data.`,
+    para4: `The data you enter is yours. By submitting you give us a license to sell this data to XYZ and others. Read <a>here</a> the full T&C.`,
+
+    para5: `We have already created a basic company profile for you based on data provided by XYZ.`,
+
+    cta: `Start`,
   },
   form: [
 
