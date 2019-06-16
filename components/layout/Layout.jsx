398aef5190d3e98f12caed328eb6bedf6b40e3a7
Chris Patuzzo <chris@patuzzo.co.uk>
2019-06-16 10:26:52 +0100

    Set the default font to ‘Source Sans Pro’

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,7 +1,12 @@
+import Head from 'next/head';
+
 import "./reset.scss";
+import "./typography.scss";
 
 const Layout = ({ children }) => <>
-  <p>Hello from Layout</p>
+  <Head>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" />
+  </Head>
 
   {children}
 </>;
